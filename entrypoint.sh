ef395e3866e9b0e3ffde775a43659d44a9fc381e
den <aditor@gmail.com>
2024-03-14 21:50:31 +0200

    remove-token fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/env bash
 
-REG_TOKEN=$(curl -sX POST -H "Authorization: token ${GITHUB_TOKEN}" https://api.github.com/orgs/"${GITHUB_ORGANIZATION}"/actions/runners/registration-token | jq .token --raw-output)
+REG_TOKEN=$(curl -sLX POST -H "Authorization: Bearer ${GITHUB_TOKEN}" -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/orgs/"${GITHUB_ORGANIZATION}"/actions/runners/registration-token | jq .token --raw-output)
 
 if [ ! -e /home/docker/actions-runner/bin ]
 then
@@ -23,7 +23,7 @@ fi
 
 cleanup() {
   echo "Removing runner..."
-  RM_TOKEN=$(curl -sX POST -H "Authorization: token ${GITHUB_TOKEN}" https://api.github.com/orgs/"${GITHUB_ORGANIZATION}"/actions/runners/remove-token | jq .token --raw-output)
+  RM_TOKEN=$(curl -sLX POST -H "Authorization: Bearer ${GITHUB_TOKEN}" -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/orgs/"${GITHUB_ORGANIZATION}"/actions/runners/remove-token | jq .token --raw-output)
   /home/docker/actions-runner/config.sh remove --token ${RM_TOKEN}
 }
 
